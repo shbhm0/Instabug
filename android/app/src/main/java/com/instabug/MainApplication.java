c08d48345620646ab421c5cb8249bf1c8af1dfa5
Alok-Gahlot <alok.gahlot@appinventiv.com>
2021-11-16 14:04:45 +0530

    Android Setup

--- android/app/src/main/java/com/instabug/MainApplication.java
+++ android/app/src/main/java/com/instabug/MainApplication.java
@@ -1,4 +1,4 @@
-package com.instabug;
+package com.instabgnew;
 
 import android.app.Application;
 import android.content.Context;
@@ -10,7 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
+import com.facebook.react.shell.MainReactPackage;
+import com.instabug.reactlibrary.RNInstabugReactnativePackage;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -26,6 +27,16 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+    //           return Arrays.<ReactPackage>asList(
+    // new MainReactPackage(),
+    // new RNInstabugReactnativePackage.Builder("ab4b6914524004360b87681ce29ca001", MainApplication.this)
+    //                         .setInvocationEvent("shake", "button", "screenshot")
+    //                         .setPrimaryColor("#1D82DC")
+    //                         .setFloatingEdge("left")
+    //                         .setFloatingButtonOffsetFromTop(250)
+    //                         .build()
+    //           )
+
           return packages;
         }
 
@@ -45,7 +56,17 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+      new RNInstabugReactnativePackage
+             .Builder("7f8fee3458928ceba7c2f2f0a0a5a327", MainApplication.this)
+             .setInvocationEvent("shake")
+             .setPrimaryColor("#1D82DC")
+             .setFloatingEdge("left")
+             .setFloatingButtonOffsetFromTop(250)
+             .build();
+     super.onCreate();
+     SoLoader.init(this, /* native exopackage */ false);
   }
+
 
   /**
    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
@@ -62,7 +83,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.instabug.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.instabgnew.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
